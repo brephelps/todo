eb2508c901092ac8bfd731a2a4819b840d2194c4
Bre Phelps <bphelps@live.com>
2019-09-14 18:16:41 +0000

    final polish

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,10 +4,17 @@ $(function() {
     // <li> tag
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<div class="view"><input class="toggle" type="checkbox"' + " data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</label></div>';
-      
-      return liElement
-    }
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+    '<div class="view"><input class="toggle" type="checkbox"' +
+      " data-id='" + task.id + "'" +
+      checkedStatus +
+      '><label>' +
+       task.title +
+       '</label></div></li>';
+
+    return liElement;
+  }
 
     // toggleTask takes in an HTML representation of
     // an event that fires from an HTML representation of
@@ -23,7 +30,12 @@ $(function() {
           task: {
             done: doneValue
           }
-        });
+        }).success(function(data) {
+          var liHtml = taskHtml(data)
+          var $li = $("#listItem-" + data.id);
+          $li.replaceWith(liHtml);
+          $('.toggle').change(toggleTask);
+        } );
       }
 
     $.get("/tasks").success(function( data ) {
